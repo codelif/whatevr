9e507591ac90beabb4324def9aaa58f16e42ac96
Harsh Sharma <harsh@codelif.in>
2026-05-06 00:45:08 +0530

    feat: improve desktop metadata

--- whatevr/build.rs
+++ whatevr/build.rs
@@ -4,10 +4,17 @@ fn main() {
         std::env::set_var("PROTOC", protoc);
     }
 
+    let proto_file = std::env::var("WHATEVR_PROTO_FILE")
+        .unwrap_or_else(|_| "../whatevrd/proto/whatevr.proto".to_string());
+    let proto_include =
+        std::env::var("WHATEVR_PROTO_INCLUDE").unwrap_or_else(|_| "../whatevrd/proto".to_string());
+
     tonic_build::configure()
         .build_server(false)
-        .compile_protos(&["../whatevrd/proto/whatevr.proto"], &["../whatevrd/proto"])
+        .compile_protos(&[proto_file.as_str()], &[proto_include.as_str()])
         .expect("failed to compile protobuf definitions");
 
-    println!("cargo:rerun-if-changed=../whatevrd/proto/whatevr.proto");
+    println!("cargo:rerun-if-env-changed=WHATEVR_PROTO_FILE");
+    println!("cargo:rerun-if-env-changed=WHATEVR_PROTO_INCLUDE");
+    println!("cargo:rerun-if-changed={proto_file}");
 }
